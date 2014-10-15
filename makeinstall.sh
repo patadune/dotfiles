0e1c067b7f06bab570cb67e57e9cabf42043d03c
re3m1 <re3m1s@gmail.com>
2014-10-15 09:19:09 +0200

    Added mutt config for ensimag mail

--- makeinstall.sh
+++ makeinstall.sh
@@ -10,7 +10,7 @@
 
 dir=~/.dotfiles # dotfiles directory
 olddir=~/.dotfiles_old # old dotfiles backup directory
-files="bashrc vimrc vim zshrc gitconfig" # list of files/folders to symlink in homedir
+files="bashrc vimrc vim zshrc gitconfig mutt" # list of files/folders to symlink in homedir
 
 ##########
 

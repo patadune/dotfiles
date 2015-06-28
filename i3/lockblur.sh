9257bdb673d83f0699e316c65c50132889786db3
re3m1 <re3m1s@gmail.com>
2015-06-28 22:42:48 +0200

    [i3] tweaked config

--- i3/lockblur.sh
+++ i3/lockblur.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -e
 
-scrot /tmp/screen_locked.png
-
-mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-
-i3lock -i /tmp/screen_locked.png && sleep 1
+if ! pgrep "i3lock" > /dev/null
+then
+    scrot /tmp/screen_locked.png
+    mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+    i3lock -i /tmp/screen_locked.png
+fi
